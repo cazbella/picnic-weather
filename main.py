863549ebc84474c64944b5094c410beee5150a30
CarolineBellaLane <carolinelane@live.co.uk>
2024-03-09 19:23:19 +0000

    pseudocodes problem

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+# pseudocode
+# Use the requests module to fetch weather data from the Open Weather API.
+# Prompt user for their location city name? postcode?
+# Use the input to construct the API request URL (same as last course)
+# Fetch the weather data for location.
+# parse JSON from the API response, such as temperature? rain? sunny? Picnic Weather!!
+# Present the weather forecast to user
+# Write the final results to a file - need to hand this in
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import requests
+import json
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
