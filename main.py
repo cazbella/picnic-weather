8811ff8c6b0a7ff862326fe2e7b6d4a7e2c335db
CarolineBellaLane <carolinelane@live.co.uk>
2024-03-13 15:12:29 +0000

    parses fetch and explains negatives in comments

--- main.py
+++ main.py
@@ -17,8 +17,13 @@
 # 1. getting the data
 import requests
 import json
+# This is my additional module
 import api_keys
 
+# I worked out how to get these into a gitignore file after lots of mistakes!
+# I can send you the api keys if requited or they are available free from
+# https://openweathermap.org/appid
+# https://developer.tomtom.com/free-maps-api
 api_key_open_weather = api_keys.OPEN_WEATHER_API_KEY
 api_key_tom_tom = api_keys.TOM_TOM_API_KEY
 
@@ -33,10 +38,12 @@ def get_weather_forecast(location):
 
     # Fetch/json format
     response = requests.get(url)
+    # I looked up about status codes
+    # https://www.moesif.com/blog/technical/monitoring/10-Error-Status-Codes-When-Building-APIs-For-The-First-Time-And-How-To-Fix-Them/
     if response.status_code == 200:
         return json.loads(response.text)
     else:
-        print("Failed to fetch weather data! - Please check the name of your city.")
+        print("Failed to fetch weather data! - Please check the name of your city or country.")
         return None
 
     # Returned data - need to parse
@@ -52,19 +59,22 @@ def get_weather_forecast(location):
 def get_POI(location, country):
     # Construct URL
     # test in browser to view what is returned
-    # need to add "&units=metric" to get in degrees c
+    # This took a lot of trial and error and api testing.
+    # Ideally I wanted an api that returned parks or green spaces but I could only find an american one.
+    # I have used tomtom before and managed to get the city and coutry to work eventually!
     url = f"https://api.tomtom.com/search/2/poiSearch/{location},{country}.json?key={api_key_tom_tom}"
 
     # Fetch/json format
     response = requests.get(url)
     if response.status_code == 200:
-        # Return parsed JSON data
+        # Return parsed data
         return json.loads(response.text)
     else:
-        print("Failed to fetch points of interest! - Please check the name of your city.")
+        print("Failed to fetch picnic places! - Please check the name of your city.")
         return None
+
 def determine_picnic_weather(temperature, wind_speed, weather_description):
-    # need to say if it's picnic weather based on the conditions
+    # Need to say if it's picnic weather based on the conditions
     if 20 <= temperature <= 36 and wind_speed < 5 and 'clear' in weather_description.lower():
         return "It is picnic weather! Grab your blanket and let's eat!"
     elif 15 <= temperature <= 25 and wind_speed < 5 and 'clouds' in weather_description.lower():
@@ -72,7 +82,8 @@ def determine_picnic_weather(temperature, wind_speed, weather_description):
     else:
         return "Eat that picnic in your living room today - stay inside!"
 
-# more help source: https://www.google.com/search?client=safari&rls=en&q=build+a+weather+app+console+python&ie=UTF-8&oe=UTF-8#fpstate=ive&vld=cid:c22b2efc,vid:Y84MGU_ZL18,st:0
+# More help source:
+# https://www.google.com/search?client=safari&rls=en&q=build+a+weather+app+console+python&ie=UTF-8&oe=UTF-8#fpstate=ive&vld=cid:c22b2efc,vid:Y84MGU_ZL18,st:0
 def present_forecast(forecast_data):
     if forecast_data:
         # Parse data (example above) - variable for each part of the description that makes it picnic weather
@@ -88,31 +99,67 @@ def present_forecast(forecast_data):
         print(f"The current wind speed is {wind_speed} m/s.")
         print(picnic_message)
 
+        # This took me a long time.
+        # I had to remove cloud descriptions because there were too many variables.
+
 
 def main():
     # Prompt for location (need to look at accuracy/specificity of location her - use reverse geocode?
     # 'input' is an inbuilt function
     location = input("Enter your city name: ")
-    country = input("Enter your country name (optional): ")
+    country = input("Enter your country name: ")
 
 
-    # Call weather forecast function with location as an argument. Forecast is fetched after user input.
+    # Call weather forecast function with location as an argument.
+    # Forecast is fetched after user input.
     forecast_data = get_weather_forecast(location)
 
     # Present forecast function recall with argument. Prints results to console.
     present_forecast(forecast_data)
 
+    # Prompt for poi - is this the best way to do this?
+    input("Would you like a picnic spot suggestion for now or the future? yes/no").lower()
+
     poi_data = get_POI(location, country)
 
     # Prints POI data
+    # if poi_data:
+    #     print("Points of Interest data:")
+    #     print(poi_data)
+
     if poi_data:
-        print("Points of Interest data:")
-        print(poi_data)
+        poi_list = poi_data['results']
+        # While loop iterates over results.
+        # Adds 1 each time to display the next in list that is IMPORTANT TOURIST ATTRACTION
+        # Variable
+        i = 0
+        # Will iterate as long as variable is less than the list of pois
+        while i < len(poi_list):
+            # Retrieves next element and stores it
+            poi = poi_list[i]
+            # https://www.w3schools.com/python/python_while_loops.asp
+            #
+            # Parse returned data
+            if 'IMPORTANT_TOURIST_ATTRACTION' in poi['poi']['classifications'][0]['code']:
+                name = poi['poi']['name']
+                postal_code = poi['address']['postalCode'][:3]  # Slicin' first three characters of the post code
+                print(f"Name: {name}, Postal Code: {postal_code}")
+                choice = input("Do you want to see another point of interest? (yes/no): ")
+                # Ensures lower case - built-in function
+                if choice.lower() == 'no':
+                    break
+            # Adds one/ increments until end of list
+            i += 1
+        else:
+            print("No important tourist attractions found.")
+    else:
+        print("No data available for the given location.")
 
     # Write results to a file syntax - need to write to file
 
 
 print("API Key:", api_key_open_weather)
+print("API Key tomtom:", api_key_tom_tom)
 
 if __name__ == "__main__":
     main()
